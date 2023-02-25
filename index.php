e97f4181c98d5e26eb79fe40ad1da01a22f6391e
AnqapCode <arifcahyogd7@gmail.com>
2023-02-25 14:36:17 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus sit recusandae ut!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
